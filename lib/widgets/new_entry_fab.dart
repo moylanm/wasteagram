4ae6ea23c298587f16ef16c1dcd27524223f5aed
Myles Moylan <moylanm@oregonstate.edu>
2023-03-17 15:18:04 -0700

    add onPressed action

--- lib/widgets/new_entry_fab.dart
+++ lib/widgets/new_entry_fab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wasteagram/screens/new_entry.dart';
 
 class NewEntryButton extends StatelessWidget {
   const NewEntryButton({super.key});
@@ -7,7 +8,12 @@ class NewEntryButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {}
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const NewEntryScreen())
+          );
+        }
     );
   }
 }
