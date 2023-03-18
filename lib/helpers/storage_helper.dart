51c82eb1a72b4ef2f0abad14db0cea8ee233c2a1
Myles Moylan <moylanm@oregonstate.edu>
2023-03-18 15:45:09 -0700

    implement StorageHelper

--- lib/helpers/storage_helper.dart
+++ lib/helpers/storage_helper.dart
@@ -1,3 +1,28 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
+
 class StorageHelper {
-  
+  static final FirebaseStorage _storageInstance = FirebaseStorage.instance;
+
+  static StorageHelper? _instance;
+
+  StorageHelper._internal() {
+    _instance = this;
+  }
+
+  factory StorageHelper() => _instance ?? StorageHelper._internal();
+
+  Future<String> uploadFile(File file) async {
+    final Reference reference = _storageInstance.ref().child(basename(file.path));
+    
+    try {
+      await reference.putFile(file);
+    } on FirebaseException catch (e) {
+      throw e.code;
+    }
+
+    return await reference.getDownloadURL();
+  }
 }
