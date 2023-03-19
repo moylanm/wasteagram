f953f97ddac4d4ee7c09eb335f38eff5f6a02adb
Myles Moylan <moylanm@oregonstate.edu>
2023-03-18 18:02:01 -0700

    implement DatabaseHelper

--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -1,3 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class DatabaseHelper {
-  
+  static final FirebaseFirestore _firestoreInstance = FirebaseFirestore.instance;
+
+  static DatabaseHelper? _instance;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+
+  Future<void> addEntry(Map<String, dynamic> entry) async {
+    await _firestoreInstance.collection('posts').add(entry);
+  }
 }
